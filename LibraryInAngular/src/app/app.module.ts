d986e3b56f6d7dc0555301996f67f66e6345f970
tanguyjob <tanguy_job@hotmail.fr>
2021-11-06 14:40:55 +0100

    creation of the home page

--- LibraryInAngular/src/app/app.module.ts
+++ LibraryInAngular/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartMenuComponent } from './cart-menu/cart-menu.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartMenuComponent,
+    TopBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'home', component: HomeComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
